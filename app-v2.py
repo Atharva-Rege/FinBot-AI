0e527db53943893bfb14c9e43c8d934b61e28b6d
Atharva Atul Rege <167877636+Atharva-Rege@users.noreply.github.com>
2024-08-26 17:51:21 +0400

    Update app-v2.py

--- app-v2.py
+++ app-v2.py
@@ -2,6 +2,10 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_PROJECT"]="FinBot-AI"
+
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 import openai
